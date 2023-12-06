c051f71ed07744c3a6fb984257234406042d0f36
Gerulis Donatas <okonecnikovaite.monika@gmail.com>
2023-12-06 13:51:18 +0200

    trecias

--- index.js
+++ index.js
@@ -213,10 +213,175 @@ let duomenys = {
     ]
 };
 const duomenysTop = duomenys.topping;
-console.log(duomenysTop);
+// console.log(duomenysTop);
 // duomenysTop.forEach((el) => console.log(`id: ${el.id}, type: ${el.type}`));
-// duomenysTop.forEach((el) => (object.entries(duomenys).forEach([KeyboardEvent, value]) =>
-//     console.log(key, value));
 
-const masyvas2 = duomenys.topping.forEach((el) => {Object.entries(duomenys).forEach(([key, value]) => console.log(key, value))})
+// const duom = duomenys.topping.forEach((el) => {Object.entries(duomenys).forEach(([key, value]) => console.log(key, value))})
 
+// duomenys.topping.forEach(item => {
+//     Object.keys(item).forEach(key => {
+//         console.log(`${key}: ${item[key]}`)
+//     })
+// })
+
+////////////////////////////////5
+
+const family = {
+    grandparents: {
+        grandma: "Marge",
+        grandpa: "Homer",
+    },
+    parents: {
+        mom: "Lisa",
+        dad:"Millhouse",
+    },
+    children: {
+        daughter: "Anne",
+        son1: "Peter",
+        son2: "Bob",
+    },
+};
+
+// for(let i in family){
+    // console.log(i)
+    // for(let a in family[i]){
+        // console.log(a);
+        // console.log(family[i]);
+        // console.log(`${i}: ${family[i][a]}`)
+        // let members = Object.values(family[i].join(", "));
+        // console.log(`${i}: ${members}`)
+//     }
+// }
+for(let i in family){
+    let members = Object.values(family[i])
+    console.log(`${i}: ${members}`);
+}
+//knygos
+let knygos = [
+    {
+      isbn: 9786098233346,
+      price: 7.99,
+      year: 2006,
+      title: "Bloga dukte",
+      pagecount: 250,
+    },
+    {
+      isbn: 9786098233391,
+      price: 7.99,
+      year: 2015,
+      title: "Mergina kuria jis pazinojo",
+      pagecount: 315,
+    },
+    {
+      isbn: 9786099609324,
+      price: 7.99,
+      year: 2019,
+      title: "Tapk ragana",
+      pagecount: 150,
+    },
+    {
+      isbn: 9786094792250,
+      price: 6.99,
+      year: 2007,
+      title: "Sfera",
+      pagecount: 450,
+    },
+    {
+      isbn: 9786094792335,
+      price: 9.99,
+      year: 2019,
+      title: "Mes susitinkame cia",
+      pagecount: 500,
+    },
+    {
+      isbn: 9786094273780,
+      price: 7.99,
+      year: 2019,
+      title: "Sunaikinimas",
+      pagecount: 509,
+    },
+    {
+      isbn: 9786098233483,
+      price: 12.99,
+      year: 2018,
+      title: "Artemide",
+      pagecount: 199,
+    },
+    {
+      isbn: 9786094273872,
+      price: 4.99,
+      year: 2015,
+      title: "Fondas ir imperija",
+      pagecount: 185,
+    },
+    {
+      isbn: 9786094273902,
+      price: 7.99,
+      year: 2019,
+      title: "Amzinybes fjordo pranasai",
+      pagecount: 333,
+    },
+    {
+      isbn: 9786094442742,
+      price: 5.99,
+      year: 2004,
+      title: "Bejegiai",
+      pagecount: 777,
+    },
+    {
+      isbn: 9786094442940,
+      price: 14.99,
+      year: 2019,
+      title: "Klajunai",
+      pagecount: 172,
+    },
+    {
+      isbn: 9786090404386,
+      price: 7.99,
+      year: 2015,
+      title: "Mergina, kuri pakliuvo i voratinkli",
+      pagecount: 356,
+    },
+  ];
+let knygosPo = [];
+
+for(let i in knygos){
+      for(let a in knygos[i]){ 
+          console.log(`${a} ${knygos[i][a]}`);
+      }
+      if(knygos[i].year > 2015){
+        knygosPo.push(knygos[i].title)
+    }
+    }
+console.log(knygosPo);
+
+//6 prat
+//1
+let  saskaitos = [45,150,225];
+let arbata2 = [];
+let total2 = [];
+function arbatpinigiai(saskaita){ 
+   saskaitos.forEach((num) => {
+       let arbata = 0;
+       let total = 0;
+       if(num <50){
+           arbata = num*0.2;
+           total = arbata + num;
+           arbata2.push(arbata);
+           total2.push(total);
+       }else if(50<=num && num<200){
+           arbata = num*0.15;
+           total = arbata+num;
+           arbata2.push(arbata);
+           total2.push(total);
+       }else{
+           arbata = num*0.1;
+           total = arbata + num;
+           arbata2.push(arbata);
+           total2.push(total);
+       }
+   });
+}
+arbatpinigiai(saskaitos)
+console.log(arbata2);
+console.log(total2);
